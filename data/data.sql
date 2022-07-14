09678fa0472a8e193ec4486f404b6136929cabcc
fabienr <fabienr@loisirsencheres.com>
2022-07-14 16:53:38 +0200

    Init database structure

--- data/data.sql
+++ data/data.sql
@@ -1,1 +1,23 @@
-SHOW DATABASES;
+USE lunapi;
+
+CREATE TABLE categories (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE programs (  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    category_id INT,
+    name VARCHAR(50),
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT FK_CategoryProgram FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE exercices (  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    program_id INT,
+    name VARCHAR(50) NOT NULL,
+    repetitions INT DEFAULT 15,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT FK_ProgramExercice FOREIGN KEY (program_id) REFERENCES programs(id)
+);
